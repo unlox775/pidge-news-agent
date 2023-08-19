b577296ca85e7a9d925f8f195d9543683f1bb0c7
Dave Buchanan <dave@joesvolcano.net>
2023-08-18 19:53:40 -0700

    Adding final report only what we didn't since last time

---
+++ src/local_functions/report_news/update_last_reported.exs
@@ -0,0 +1,13 @@
+def function(report, last_reported) do
+  last_reported = if last_reported, do: last_reported, else: %{}
+  previous_report = last_reported.last_report
+
+  %{}
+  |> Map.put(:last_report_date, Date.to_iso8601(Date.utc_today()))
+  |> Map.put(:last_report, report)
+  |> Map.put(:previous_reports, Map.put(
+    Map.get(last_reported, :previous_reports, %{}),
+    previous_report.last_report_date,
+    previous_report
+    ))
+end
